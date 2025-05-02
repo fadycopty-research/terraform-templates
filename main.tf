878697856f535b512fb1badd99fef036bf0a55ef
Fady-Copty <139892253+Fady-Copty@users.noreply.github.com>
2025-05-02 11:24:31 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
+}
+
+resource "azurerm_storage_container" "public_container" {
+  name                  = "public-container"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "blob" 
+}
